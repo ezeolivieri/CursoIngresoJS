25c9d290f84ca8c5214af89f51ec09f9adb19650
EzequielO1531 <ezequielo@epidataconsulting.com>
2020-07-17 22:12:54 -0300

    TPs 2 y 3 completos y enviados

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,12 +7,84 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+     // Definicion de variables
+     var largoTerreno;
+     var anchoTerreno;
+     var perimetro;
+     var cantidadAlambre;
+
+     // Asignacion de valores
+     largoTerreno = document.getElementById('txtIdLargo').value;
+     anchoTerreno = document.getElementById('txtIdAncho').value;
+
+     // Parseo a Entero
+     largoTerreno = parseInt(largoTerreno);
+     anchoTerreno = parseInt(anchoTerreno);
+
+     // Calculo
+     perimetro = (largoTerreno * 2) + (anchoTerreno * 2);
+     cantidadAlambre = perimetro * 3;
+
+     // Muestreo de datos
+     alert("Se debe comprar en total "+ cantidadAlambre +" de alambre");
+
 }
 function Circulo () 
 {
+
+     // Definicion de variables
+     var radioTerreno;
+     var diametro;
+     var pi;
+     var perimetro;
+     var cantidadAlambre;
+
+     // Asignacion de valores
+     radioTerreno = document.getElementById('txtIdRadio').value;
+     pi = 3.14;
+
+     // Parseo a entero
+     radioTerreno = parseInt(radioTerreno);
+
+     // Calculo
+     diametro = radioTerreno * 2;
+     perimetro = diametro * pi;
+     cantidadAlambre = perimetro * 3;
+
+     // Muestreo de datos
+     alert("Se debe comprar en total "+cantidadAlambre+" de alambre");
 	
 }
 function Materiales () 
 {
+
+     // Definicion de variables
+     var largoTerreno;
+     var anchoTerreno;
+     var perimetro;
+     var cantidadLados;
+     var medidaDeLados;
+     var bolsasDeCemento;
+     var bolsasDeCal;
+
+     // Asignacion de valores
+     largoTerreno = document.getElementById('txtIdLargo').value;
+     anchoTerreno = document.getElementById('txtIdAncho').value;
+     bolsasDeCemento = 2;
+     bolsasDeCal = 3;
+     cantidadLados = 4;
+
+     // Parseo a Entero
+     largoTerreno = parseInt(largoTerreno);
+     anchoTerreno = parseInt(anchoTerreno);
+
+     // Calculo
+     perimetro = (largoTerreno * 2) + (anchoTerreno * 2);
+     medidaDeLados = perimetro / cantidadLados;
+     bolsasDeCemento = bolsasDeCemento * medidaDeLados;
+     bolsasDeCal = bolsasDeCal * medidaDeLados;
+
+     // Muestreo de datos
+     alert("Se deben comprar "+bolsasDeCemento+" bolsas de cemento y "+bolsasDeCal+" de cal");
 	
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,58 @@
+// Olivieri Ezequiel
+
 /*3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+
 function FahrenheitCentigrados () 
 {
+
+     // Definicion de variables
+     var temperatura;
+     var puntoDeCongelamiento;
+     var equivalenteEnCentigrados;
+     var centigrados;
+
+     // Asignacion de valores
+     temperatura = document.getElementById('txtIdTemperatura').value;
+     puntoDeCongelamiento = 32;
+     equivalenteEnCentigrados = 5/9;
+
+     // Parseo a Entero
+     temperatura = parseInt(temperatura);
+
+     // Calculos
+     centigrados = temperatura - puntoDeCongelamiento;
+     centigrados = centigrados * equivalenteEnCentigrados;
+
+     // Muestreo de datos
+     alert( temperatura+" Fahrenheit son "+centigrados+" centígrados" );
 	
 }
 
 function CentigradosFahrenheit () 
 {
+
+     // Definicion de variables
+     var temperatura;
+     var puntoDeCongelamiento;
+     var equivalenteEnFahrenheit;
+     var fahrenheit;
+
+     // Asignacion de valores
+     temperatura = document.getElementById('txtIdTemperatura').value;
+     equivalenteEnFahrenheit = 9/5;
+     puntoDeCongelamiento = 32;
+
+     // Parseo a Entero
+     temperatura = parseInt(temperatura);
+
+     // Calculos
+     fahrenheit = temperatura * equivalenteEnFahrenheit;
+     fahrenheit = fahrenheit + puntoDeCongelamiento;
+
+     // Muestreo de datos
+     alert( temperatura+" centígrados son "+fahrenheit+" Fahrenheit" );
 	
 }
